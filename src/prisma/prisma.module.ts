3091ece55826c5eeb96f0d6376a1ad001f035f8b
Julio Filizzola <juliofilizzola@Hotmail.com>
2023-12-21 23:39:13 -0300

    modify module

--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -1,9 +1,8 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
-import { PrismaController } from './prisma.controller';
 
+@Global()
 @Module({
-  controllers: [PrismaController],
   providers: [PrismaService],
 })
 export class PrismaModule {}
