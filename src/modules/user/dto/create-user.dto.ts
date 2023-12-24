f2c2eb2b7de3aee0e56af02912bbc702d9e86346
Julio Filizzola <juliofilizzola@Hotmail.com>
2023-12-24 10:08:00 -0300

    update create user dto

--- src/modules/user/dto/create-user.dto.ts
+++ src/modules/user/dto/create-user.dto.ts
@@ -1,1 +1,36 @@
-export class CreateUserDto {}
+import { IsEmail, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString({
+    message: 'name is invalid',
+  })
+  @IsNotEmpty({
+    message: 'name is required',
+  })
+  name: string;
+
+  @IsEmail(
+    {},
+    {
+      message: 'email is invalid',
+    },
+  )
+  @IsNotEmpty({
+    message: 'email is required',
+  })
+  email: string;
+
+  @IsString({
+    message: 'phone is invalid',
+  })
+  @IsOptional()
+  phone: string;
+
+  @IsString({
+    message: 'document is invalid',
+  })
+  @IsNotEmpty({
+    message: 'document is required',
+  })
+  document: string;
+}
