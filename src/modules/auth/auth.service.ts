66bdd5fc67d71e21a365d23c4077d4266b17e24c
Julio Filizzola <juliofilizzola@Hotmail.com>
2023-12-23 22:59:41 -0300

    modify func
    modify func create user

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,15 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
+import { PrismaService } from '../../prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
+import { validDocument } from '../../utils/validation/valid-document';
+import { formatDocument } from '../../utils/format/format-document';
+import { TypeRole } from '@prisma/client';
 
 @Injectable()
 export class AuthService {
+  constructor(private readonly prismaService: PrismaService) {}
   create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
+    const validDoc = validDocument(createAuthDto.document);
+
+    if (!validDoc) {
+      throw new BadRequestException({
+        message: 'document is invalid',
+      });
+    }
+
+    return this.prismaService.user.create({
+      data: {
+        email: createAuthDto.email,
+        type: TypeRole.client,
+        document: formatDocument(createAuthDto.document),
+        phone: createAuthDto.phone,
+        name: createAuthDto.name,
+        auth: {
+          create: {
+            password: bcrypt.hashSync(createAuthDto.password, 10),
+          },
+        },
+      },
+    });
   }
 
   findAll() {
-    return `This action returns all auth`;
+    return 'This action returns all auth';
   }
 
   findOne(id: number) {
