dc594d8b8d12d08968fe50f954c949fe4fb10856
Julio Filizzola <juliofilizzola@Hotmail.com>
2023-12-25 22:06:41 -0300

    modify router and add func from reserve

--- src/modules/reserve/reserve.controller.ts
+++ src/modules/reserve/reserve.controller.ts
@@ -1,34 +1,53 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+  Req,
+} from '@nestjs/common';
 import { ReserveService } from './reserve.service';
 import { CreateReserveDto } from './dto/create-reserve.dto';
 import { UpdateReserveDto } from './dto/update-reserve.dto';
+import { UserAuth } from '../../model/userAuth';
+import { Roles } from '../../decorators/roles.decorator';
+import { TypeRoles } from '../../roles/role';
+import { PaginationParams } from '../../utils/paginations/type';
 
 @Controller('reserve')
 export class ReserveController {
   constructor(private readonly reserveService: ReserveService) {}
 
   @Post()
-  create(@Body() createReserveDto: CreateReserveDto) {
-    return this.reserveService.create(createReserveDto);
+  @Roles([TypeRoles.client, TypeRoles.admin])
+  create(@Body() createReserveDto: CreateReserveDto, @Req() req: UserAuth) {
+    return this.reserveService.create(createReserveDto, req.user.sub);
   }
 
   @Get()
-  findAll() {
-    return this.reserveService.findAll();
+  findAll(@Query() { page, limit }: PaginationParams) {
+    const pagination: PaginationParams =
+      page && limit ? { page: Number(page), limit: Number(limit) } : undefined;
+    return this.reserveService.findAll(pagination);
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.reserveService.findOne(+id);
+    return this.reserveService.findOne(id);
   }
 
   @Patch(':id')
+  @Roles([TypeRoles.admin, TypeRoles.client])
   update(@Param('id') id: string, @Body() updateReserveDto: UpdateReserveDto) {
-    return this.reserveService.update(+id, updateReserveDto);
+    return this.reserveService.update(id, updateReserveDto);
   }
 
   @Delete(':id')
+  @Roles([TypeRoles.admin, TypeRoles.client])
   remove(@Param('id') id: string) {
-    return this.reserveService.remove(+id);
+    return this.reserveService.remove(id);
   }
 }
--- src/modules/reserve/reserve.service.ts
+++ src/modules/reserve/reserve.service.ts
@@ -1,26 +1,228 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateReserveDto } from './dto/create-reserve.dto';
 import { UpdateReserveDto } from './dto/update-reserve.dto';
+import { PrismaService } from '../../prisma/prisma.service';
+import { addMinutes } from 'date-fns';
+import { Prisma, Reserve, TypeRole } from '@prisma/client';
+import { paginateResponse } from '../../utils/paginations/pagination';
+import { PaginationParams } from '../../utils/paginations/type';
 
+interface IValidationBroker {
+  idBroker: string;
+  duration: number;
+  appointment: Date;
+  date: Date;
+}
 @Injectable()
 export class ReserveService {
-  create(createReserveDto: CreateReserveDto) {
-    return 'This action adds a new reserve';
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async create(createReserveDto: CreateReserveDto, userId: string) {
+    const user = await this.prismaService.user.findFirst({
+      where: {
+        id: userId,
+      },
+    });
+
+    if (!user) {
+      throw new NotFoundException({
+        message: 'User not found',
+      });
+    }
+
+    const validation = this.validationBroker({
+      appointment: createReserveDto.appointment,
+      date: createReserveDto.date,
+      duration: createReserveDto.duration,
+      idBroker: createReserveDto.idBroker,
+    });
+
+    if (!validation) {
+      throw new BadRequestException({
+        message: 'Reservation with this duration is not available',
+      });
+    }
+
+    return this.prismaService.reserve.create({
+      data: {
+        broker: {
+          connect: {
+            id: createReserveDto.idBroker,
+          },
+        },
+        client: {
+          connect: {
+            id: user.id,
+          },
+        },
+        appointment: createReserveDto.appointment,
+        date: createReserveDto.date,
+        duration: createReserveDto.duration,
+        description: createReserveDto.description,
+        title: createReserveDto.title,
+      },
+    });
   }
 
-  findAll() {
-    return `This action returns all reserve`;
+  async findAll(pagination?: PaginationParams) {
+    if (pagination) {
+      const { page, limit } = pagination;
+      const response = await this.prismaService.$transaction([
+        this.prismaService.reserve.count({}),
+        this.prismaService.reserve.findMany({
+          where: {},
+          skip: (page - 1) * limit,
+          take: limit,
+          orderBy: {
+            createdAt: 'desc',
+          },
+        }),
+      ]);
+
+      return paginateResponse<Reserve>({
+        page,
+        limit,
+        total: response[0],
+        result: response[1],
+      });
+    }
+
+    return this.prismaService.reserve.findMany({});
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} reserve`;
+  async findOne(id: string) {
+    const reserve = await this.prismaService.reserve.findFirst({
+      where: {
+        id,
+      },
+    });
+
+    if (!reserve) {
+      throw new NotFoundException({
+        message: 'reserve not found',
+      });
+    }
+
+    return reserve;
   }
 
-  update(id: number, updateReserveDto: UpdateReserveDto) {
-    return `This action updates a #${id} reserve`;
+  async update(id: string, updateReserveDto: UpdateReserveDto) {
+    const reserve = await this.prismaService.reserve.findFirst({
+      where: {
+        id,
+      },
+    });
+
+    if (!reserve) {
+      throw new NotFoundException({
+        message: 'reserve not found',
+      });
+    }
+
+    let reserveUpdateInput: Prisma.ReserveUpdateInput = {
+      appointment: updateReserveDto?.appointment || reserve.appointment,
+      date: updateReserveDto?.date || reserve.date,
+      duration: updateReserveDto?.duration || reserve.duration,
+      description: updateReserveDto?.description || reserve.description,
+      title: updateReserveDto?.title || reserve.title,
+    };
+
+    if (updateReserveDto?.idBroker) {
+      const validation = this.validationBroker({
+        appointment: updateReserveDto?.appointment || reserve.appointment,
+        date: updateReserveDto?.date || reserve.date,
+        duration: updateReserveDto?.duration || reserve.duration,
+        idBroker: updateReserveDto?.idBroker || reserve.brokerId,
+      });
+
+      if (!validation) {
+        throw new BadRequestException({
+          message: 'Reservation with this duration is not available',
+        });
+      }
+
+      reserveUpdateInput = {
+        ...reserveUpdateInput,
+        broker: {
+          connect: {
+            id: updateReserveDto.idBroker,
+          },
+        },
+      };
+    }
+
+    return this.prismaService.reserve.update({
+      where: {
+        id,
+      },
+      data: reserveUpdateInput,
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} reserve`;
+  async remove(id: string) {
+    const reserve = await this.prismaService.reserve.findFirst({
+      where: {
+        id,
+      },
+    });
+
+    if (!reserve) {
+      throw new NotFoundException({
+        message: 'reserve not found',
+      });
+    }
+
+    return this.prismaService.reserve.delete({
+      where: {
+        id,
+      },
+    });
+  }
+
+  private async validationBroker(data: IValidationBroker): Promise<boolean> {
+    const broker = await this.prismaService.user.findFirst({
+      where: {
+        id: data.idBroker,
+      },
+    });
+
+    if (!broker || broker.type !== TypeRole.brokers) {
+      throw new NotFoundException({
+        message: 'Broker not found',
+      });
+    }
+
+    if (data.duration > 120 || data.duration < 30) {
+      throw new BadRequestException({
+        message:
+          'Invalid duration: Max duration 120 minutes, Min duration 30 minutes',
+      });
+    }
+
+    const durationReserve = addMinutes(data.appointment, data.duration);
+
+    const overlappingReservations = await this.prismaService.reserve.findMany({
+      where: {
+        broker: {
+          id: broker.id,
+        },
+        AND: {
+          date: data.date,
+          appointment: {
+            gte: data.appointment,
+            lte: durationReserve,
+          },
+        },
+      },
+    });
+
+    const validDuration = overlappingReservations.filter(
+      (value) => durationReserve > value.appointment,
+    );
+    return !!validDuration.length;
   }
 }
