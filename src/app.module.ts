5974eeac32721231a94ea9660cd5f0522c6c8a80
Julio Filizzola <juliofilizzola@Hotmail.com>
2023-12-27 17:04:50 -0300

    CREATE HANDLING ERRORS

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { UserModule } from './modules/user/user.module';
-import { APP_GUARD } from '@nestjs/core';
+import { APP_FILTER, APP_GUARD } from '@nestjs/core';
 import { JwtAuthGuard } from './modules/auth/guards/jwt-auth.guard';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaService } from './prisma/prisma.service';
 import { ReserveModule } from './modules/reserve/reserve.module';
+import { GlobalExceptionFilter } from './interceptor/GlobalExceptionFilter';
 
 @Module({
   imports: [
@@ -23,6 +24,10 @@ import { ReserveModule } from './modules/reserve/reserve.module';
     {
       provide: APP_GUARD,
       useClass: JwtAuthGuard,
+    },
+    {
+      provide: APP_FILTER,
+      useClass: GlobalExceptionFilter,
     },
   ],
   exports: [PrismaModule],
